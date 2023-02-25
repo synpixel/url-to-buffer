4fd85cc0e82741397a5db64e9ce11f21439dfc29
DogeyStudio <syntaxwastaken2@gmail.com>
2023-02-25 22:48:06 +0100

    Import puppeter-chromium-resolver

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -2,11 +2,15 @@ type Body = any;
 
 import { FastifyPluginAsync } from 'fastify';
 import puppeteer from 'puppeteer';
+const PCR: any = import('puppeteer-chromium-resolver');
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.post('/', async function (request, reply) {
+    const stats = await PCR({});
+
     const browser = await puppeteer.launch({
-      args: ['--no-sandbox']
+      args: ['--no-sandbox'],
+      executablePath: stats.executablePath
     });
     const page = await browser.newPage();
 
