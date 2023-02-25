3f566c76aaa2b9b15f116cd5c9c9070448d9ee47
DogeyStudio <syntaxwastaken2@gmail.com>
2023-02-25 19:36:28 +0100

    Fix typescript

--- out/app.js
+++ out/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -8,25 +17,27 @@ const puppeteer_1 = __importDefault(require("puppeteer"));
 const server = (0, fastify_1.default)({
     logger: true
 });
-server.post('/', async (request, reply) => {
-    const browser = await puppeteer_1.default.launch();
-    const page = await browser.newPage();
+server.post('/', (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
+    const browser = yield puppeteer_1.default.launch();
+    const page = yield browser.newPage();
     const body = request.body;
-    await page.goto(body.url);
-    await page.setViewport({ width: 1920, height: 1080 });
-    await page.screenshot().then((buffer) => {
+    yield page.goto(body.url);
+    yield page.setViewport({ width: 1920, height: 1080 });
+    yield page.screenshot().then((buffer) => {
         reply.send(buffer);
     });
-    await browser.close();
+    yield browser.close();
     return reply;
-});
-async function start() {
-    try {
-        await server.listen({ host: 'localhost', port: 3000 });
-    }
-    catch (err) {
-        server.log.error(err);
-        process.exit(1);
-    }
+}));
+function start() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield server.listen({ host: '0.0.0.0', port: 3000 });
+        }
+        catch (err) {
+            server.log.error(err);
+            process.exit(1);
+        }
+    });
 }
 start();
